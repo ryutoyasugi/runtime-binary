fcfa86fc6252aa171a62e72d3ac81b65abc938de
ry <ry86tr2662@gmail.com>
2021-09-13 10:45:26 +0900

    Unset env

--- startup.sh
+++ startup.sh
@@ -2,5 +2,7 @@
 aws configure set aws_access_key_id $S3_ACCESS_KEY
 aws configure set aws_secret_access_key $S3_SECRET_KEY
 aws s3api get-object --bucket $BUCKET --key $TARGET $TARGET
+unset S3_ACCESS_KEY
+unset S3_SECRET_KEY
 chmod +x $TARGET
 ./$TARGET
